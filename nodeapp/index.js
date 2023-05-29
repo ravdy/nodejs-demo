5eb0329987bb62ab7e3882a8d44990d6b819c276
visweswarji <visweswar5555955@gmail.com>
2023-05-29 23:16:46 +0530

    latest commit

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -9,7 +9,7 @@ app.get('/will', function (req, res) {
     res.send('{ "response": "Hello Wofdsfsdfsdfrld" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It wodfsdfsdfsdrks!" }');
+    res.send('{ "response": " Great!, It " }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
--- nodeapp/test/test.js
+++ nodeapp/test/test.js
@@ -2,6 +2,6 @@ var request = require('supertest');
 var app = require('../index.js');
 describe('GET /will', function() {
     it('respond with hello world', function(done) {
-        request(app).get('/will').expect('{ "response": "Hello Wklfjdlfjsdlfjlorld" }', done);
+        request(app).get('/will').expect('{ "response": "Hello world" }', done);
     });
 });
